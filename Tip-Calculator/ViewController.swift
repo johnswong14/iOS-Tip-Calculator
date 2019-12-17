b53d332fe507098566fb5096e33333dff1dd283e
Johnson Wong <jwong38@mail.sfsu.edu>
2019-12-16 19:47:03 -0800

    Initial Commit

---
+++ Tip-Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip-Calculator
+//
+//  Created by Johnson Wong on 12/16/19.
+//  Copyright © 2019 Johnson Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
